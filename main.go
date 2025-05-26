199b3cd31335fb0820726219afe57a08a804ea80
jacktrusler <jtrus93@gmail.com>
2025-05-26 14:12:21 -0400

    add chi router

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+const PORT = 8080
 
 func main() {
-	fmt.Println("Gamertime")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
+	})
+
+	fmt.Printf("Listening on port :%d\n", PORT)
+	http.ListenAndServe(fmt.Sprintf(":%d", PORT), r)
 }
