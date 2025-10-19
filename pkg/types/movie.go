2153d3deb5c70a8d678b9972b10e4556b1744537
Sam Swindell <swindesr@gmail.com>
2025-10-19 12:01:57 -0400

    Add comment

--- pkg/types/movie.go
+++ pkg/types/movie.go
@@ -1,5 +1,7 @@
 package types
 
+// Movie is our internal representation of a movie that contains metadata expected on
+// any movie, whether it comes from Jellyfin or Plex, etc.
 type Movie struct {
 	CommunityRating float64
 	CriticRating    int
